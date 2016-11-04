af7d3e9c39d9994b78762e6e634de97eea99fa86
Konrad Kapp <k_kapp@yahoo.com>
2016-11-04 11:58:30 +0200

    Fix corner case in max_subarr

--- hackerrank_complete/max_subarr/main.cpp
+++ hackerrank_complete/max_subarr/main.cpp
@@ -43,16 +43,10 @@ info_list reduce(const list<long> &arr)
 
 	for (auto &val : arr)
 	{
-		/*
 		if ((long long)val * (long long)curr_sum >= 0)
 		{
-			curr_sum += val;
-		}
-		*/
-		if (((val >= 0) && (curr_sum >= 0)) || 
-			((val <= 0) && (curr_sum <= 0)))
-		{
-			curr_sum += val;
+			if (!((val < 0) && (curr_sum == 0)))
+				curr_sum += val;
 		}
 		else
 		{
@@ -66,51 +60,9 @@ info_list reduce(const list<long> &arr)
 	reduced.push_back({curr_sum, {curr_range.first, idx - 1}});
 
 
-	if (!check_dupl(reduced))
-	{
-		cout << "Problem detected" << endl;
-	}
-
 	return reduced;
 }
 
-/*
-vector<info_list::iterator> reduce2(info_list &arr)
-{
-	bool no_erase;
-
-	vector<info_list::iterator> check_iters;
-
-	for (auto iter = arr.begin(); iter != arr.end();)
-	{
-		no_erase = true;
-		if (iter->first > 0)
-		{
-			if ((next(iter, 1) != arr.end()) && (next(iter, 2) != arr.end()))
-			{
-				if ((next(iter, 2)->first > abs(next(iter, 1)->first)) 
-					&& (iter->first > abs(next(iter, 1)->first)))
-				{
-					iter->first = next(iter, 2)->first + next(iter, 1)->first + iter->first;
-					iter->second.second = next(iter, 2)->second.second;
-					arr.erase(next(iter, 1), next(iter, 3));
-					no_erase = false;
-					if (iter != arr.begin())
-					{
-						check_iters.push_back(next(iter, -1));
-					}
-				}
-			}
-		}
-		if (no_erase)
-		{
-			advance(iter, 1);
-		}
-	}
-
-	return check_iters;
-}
-*/
 void reduce2(info_list &arr)
 {
 	bool no_erase;
@@ -138,9 +90,6 @@ void reduce2(info_list &arr)
 			advance(iter, 1);
 		}
 	}
-
-
-
 }
 
 
@@ -158,27 +107,12 @@ pair<info_list, info_list> reduce_all(const list<long> arr)
 {
 	info_list reduced = reduce(arr);
 	auto reduced_init = reduced;
-	if (!check_dupl(reduced))
-	{
-		cout << "Warning: duplicates found!!" << endl;
-		cout << "reduced size: " << reduced.size() << endl;
-		/*
-		print_info_list(reduced);
-		for (auto &info_pair : reduced)
-		{
-			cout << info_pair.second.first << ", " << info_pair.second.second << ": " << sum_range(info_pair.second.first, info_pair.second.second, arr) << endl;
-		}
-		*/
-	}
 	unsigned int prev_size;
 	do
 	{
 		prev_size = reduced.size();
 		reduce2(reduced);
-		//cout << "reducing again" << endl;
 	} while (prev_size != reduced.size());
-	//cout << "final size: " << reduced.size() << endl;
-	//print_info_list(reduced);
 	return {reduced, reduced_init};
 }
 
@@ -280,7 +214,6 @@ int main(int argc, char * argv [])
 			int overall_max = all_positive(reduced_pair.second);
 			outs.push_back({max_info.first, overall_max});
 		}
-		//cout << "max_info: " << max_info.first << ", (" << max_info.second.first << ", " << max_info.second.second << ")" << endl;
 	}
 	for (auto &pr : outs)
 	{
