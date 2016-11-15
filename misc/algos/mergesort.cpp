2a27d08418c6a3dec99d7303390f256187f90978
Konrad Kapp <k_kapp@yahoo.com>
2016-11-16 01:19:52 +0200

    Make better comments and variable names for mergesort.cpp

--- misc/algos/mergesort.cpp
+++ misc/algos/mergesort.cpp
@@ -13,12 +13,14 @@ using namespace std;
 
 /*
 	helper function for mergesort. merges two adjacent, ordered linked lists into
-	a single ordered linked list
+		a single ordered linked list. Linked lists were used for efficient removal of
+		elements anywhere in the container. Random access is also not needed, which
+		makes linked lists a good choice
 */
 template<typename T>
 list<T> merge_two(list<T> &first, list<T> &second, function<bool(T, T)> comp)
 {
-	list<T> new_vec;
+	list<T> new_list;
 	int fwd_count = 0;
 
 	for (auto first_iter = first.begin(); first_iter != first.end(); )
@@ -30,35 +32,42 @@ list<T> merge_two(list<T> &first, list<T> &second, function<bool(T, T)> comp)
 		{
 			if (comp(*first_iter, *second_iter))
 			{
-				new_vec.push_back(*first_iter);
+				new_list.push_back(*first_iter);
 				first_iter = first.erase(first_iter);
 				inserted = true;
 				break;
 			}
 			else
 			{
-				new_vec.push_back(*second_iter);
+				new_list.push_back(*second_iter);
 				second_iter = second.erase(second_iter);
 				fwd_count++;
 			}
 		}
 		if (!inserted)
 		{
-			new_vec.push_back(*first_iter);
+			new_list.push_back(*first_iter);
 			advance(first_iter, 1);
 		}
 	}
 
 	for (auto second_iter = second.begin(); second_iter != second.end();)
 	{
-		new_vec.push_back(*second_iter);
+		new_list.push_back(*second_iter);
 		second_iter = second.erase(second_iter);
 	}
 
 
-	return new_vec;
+	return new_list;
 }
 
+/*
+	the main mergesort function. the user may write his/her own comparison function, and any container,
+		holding any type of value/object, may be used, as long as
+			it contains the value_type attribute
+			it implements iterators, and begin and end functions for use with iterators
+			it has a constructor that accepts a range of iterators for initialisation
+*/
 template<typename T>
 void mergesort(T &container, function<bool(typename T::value_type, typename T::value_type)> comp_func)
 {
